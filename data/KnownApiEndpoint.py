7966d01c4d2b366dad90aaef4a7e3867127d1c4d
Krzysztof Saczuk <zakku@zakku.eu>
2022-01-07 15:15:55 +0100

    refactor: remove dotmap dependency and its usages

--- data/KnownApiEndpoint.py
+++ data/KnownApiEndpoint.py
@@ -1,11 +1,14 @@
 """ Contains KnownApiEndpoint type """
 from typing import Pattern
 
-from dotmap import DotMap
-
 
 # pylint: disable=too-few-public-methods
-class KnownApiEndpoint(DotMap):
+class KnownApiEndpoint:
     """ Holds data used to identify known API endpoints """
     regex: Pattern[str]
     path: str
+
+    def __init__(self, regex: Pattern[str], path: str) -> None:
+        super().__init__()
+        self.regex = regex
+        self.path = path
