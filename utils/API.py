5fd791a8d1471d8847f617a6d438e54968e2a17e
Krzysztof Saczuk <zakku@zakku.eu>
2022-01-14 13:17:48 +0100

    style(API): replace single quotes with double quotes

--- utils/API.py
+++ utils/API.py
@@ -15,38 +15,42 @@ class API(Site):
 
     def site_init(self):
         if self.initialized:
-            info = self.get('query', meta='userinfo', uiprop='groups|rights')
-            userinfo = info['query']['userinfo']
-            self.username = userinfo['name']
-            self.groups = userinfo.get('groups', [])
-            self.rights = userinfo.get('rights', [])
+            info = self.get(action="query", meta=["userinfo"], uiprop=["groups", "rights"])
+            userinfo = info["query"]["userinfo"]
+            self.username = userinfo["name"]
+            self.groups = userinfo.get("groups", [])
+            self.rights = userinfo.get("rights", [])
             self.tokens = {}
             return
 
-        meta = self.get('query', meta='siteinfo|userinfo',
-                        siprop='general|namespaces', uiprop='groups|rights',
-                        retry_on_error=False)
+        meta = self.get(
+            action="query",
+            meta=["siteinfo", "userinfo"],
+            siprop=["general", "namespaces"],
+            uiprop=["groups", "rights"],
+            retry_on_error=False
+        )
 
         # Extract site info
-        self.site = meta['query']['general']
+        self.site = meta["query"]["general"]
         self.namespaces = {
-            namespace['id']: WikiNamespace(
+            namespace["id"]: WikiNamespace(
                 namespace["id"],
                 "Main" if not namespace["*"] and namespace["id"] == 0 else namespace["*"],
                 "content" in namespace
             )
-            for namespace in meta['query']['namespaces'].values()
+            for namespace in meta["query"]["namespaces"].values()
         }
-        self.writeapi = 'writeapi' in self.site
+        self.writeapi = "writeapi" in self.site
 
-        self.version = self.version_tuple_from_generator(self.site['generator'])
+        self.version = self.version_tuple_from_generator(self.site["generator"])
 
         # Require MediaWiki version >= 1.16
         self.require(1, 16)
 
         # User info
-        userinfo = meta['query']['userinfo']
-        self.username = userinfo['name']
-        self.groups = userinfo.get('groups', [])
-        self.rights = userinfo.get('rights', [])
+        userinfo = meta["query"]["userinfo"]
+        self.username = userinfo["name"]
+        self.groups = userinfo.get("groups", [])
+        self.rights = userinfo.get("rights", [])
         self.initialized = True
